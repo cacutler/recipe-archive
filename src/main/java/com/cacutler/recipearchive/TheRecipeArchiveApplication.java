0caab6e5573afad4563b69de43526ca29f836d2b
cacutler <116305038+cacutler@users.noreply.github.com>
2026-01-25 10:21:29 -0700

    Initial commit

---
+++ src/main/java/com/cacutler/recipearchive/TheRecipeArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.cacutler.recipearchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRecipeArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRecipeArchiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cacutler/recipearchive/TheRecipeArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cacutler.recipearchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRecipeArchiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
