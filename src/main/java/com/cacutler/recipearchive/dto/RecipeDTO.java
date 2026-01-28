4fbdae67d95a435330d18d6ebcee8bf05c2962a4
cacutler <116305038+cacutler@users.noreply.github.com>
2026-01-28 13:34:28 -0700

    Created inital DTOs for users and recipes

--- src/main/java/com/cacutler/recipearchive/dto/RecipeDTO.java
+++ src/main/java/com/cacutler/recipearchive/dto/RecipeDTO.java
@@ -1,5 +1,22 @@
 package com.cacutler.recipearchive.dto;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RecipeDTO {
-    
+    private Long id;
+    private Long userId;//May need to add a private string username attribute
+    private String title;
+    private String description;
+    private String ingredients;
+    private String instructions;
+    private String allergies;
+    private Integer prepTime;
+    private Integer cookingTime;
+    private Integer servings;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
--- src/main/java/com/cacutler/recipearchive/dto/UserDTO.java
+++ src/main/java/com/cacutler/recipearchive/dto/UserDTO.java
@@ -1,5 +1,17 @@
 package com.cacutler.recipearchive.dto;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
-    
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
